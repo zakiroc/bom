bce91c98373e224aeaf9a5fff00535c892f4e9b4
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-07-01 22:22:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let body = document.querySelector('body')
+
+let h1 = document.querySelector('.h1')
+let pr = prompt('ismingizni kiriting')
+console.log(pr);
+h1.innerHTML = pr
+
+
+
+
+body.style.backgroundColor = "black"
+h1.style.color = "red"
+h1.style.textAlign = "center"
+h1.style.fontSize = "100px"
+h1.style.marginTop = "250px"
